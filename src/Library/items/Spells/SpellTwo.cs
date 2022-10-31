df4f0994a52189af816cb063c85e9512d1e8aff6
juang0nz <101676463+juang0nz@users.noreply.github.com>
2022-10-31 20:31:54 -0300

    Se agrega Spells

---
+++ src/Library/items/Spells/SpellTwo.cs
@@ -0,0 +1,21 @@
+namespace RoleplayGame
+{
+    public class SpellTwo: ISpell
+    {
+        public int AttackValue
+        {
+            get
+            {
+                return 60;
+            }
+        }
+
+        public int DefenseValue
+        {
+            get
+            {
+                return 60;
+            }
+        }
+    }
+}
