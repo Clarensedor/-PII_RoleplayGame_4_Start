b83286f60102160ee93fd14d1ae078a07d9986c2
Facu <facupintos@hotmail.com>
2022-11-02 04:00:26 -0300

    Se agregan items Stone y PrisonSpear

---
+++ src/Library/items/Attack Items/Stone.cs
@@ -0,0 +1,13 @@
+namespace RoleplayGame
+{
+    public class Stone: IAttackItem
+    {
+        public int AttackValue 
+        {
+            get
+            {
+                return 5;
+            } 
+        }
+    }
+}
