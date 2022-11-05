bc56de6f616f040431998861eb3b2c2198728d2b
Anthony Pereira <101676526+Clarensedor@users.noreply.github.com>
2022-11-05 20:33:11 -0300

    fix chapter one

--- src/Library/scenarios/ChapterOne.cs
+++ src/Library/scenarios/ChapterOne.cs
@@ -10,18 +10,27 @@ namespace RoleplayGame.Scenarios
       public List<IItem> items;
 
 
+
+
        public void Setup(){
-        
+
+
         //creamos las tres instancias de characters del character factory
         var martinsito = CharacterFactory.GetCharacter(CharacterType.Elf, "martinsito");
         var pintonio = CharacterFactory.GetCharacter(CharacterType.Human, "Pintonio");
         var juancito = CharacterFactory.GetCharacter(CharacterType.Wizard, "Juancito");
+        
+        //crea las instancias de items del items factory
+        var stick = ItemFactory.GetItem(ItemType.Robes);
+       
+  
+        //agrego los character a la lista
         characters.Add(martinsito);
         characters.Add(pintonio);
         characters.Add(juancito);
 
 
-         Random random = new Random();
+         //Random random = new Random();
          //int numeroAleatorio = random.Next(0, );
          //Martinsito.AddItem(numeroAletorio);
 
@@ -30,7 +39,8 @@ namespace RoleplayGame.Scenarios
        }
 
        public void Run(){
-        martinsito
+          characters[0].ReceiveAttack(characters[1]);
+          
 
         }
 
