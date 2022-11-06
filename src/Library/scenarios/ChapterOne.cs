4c7faa7209aa5353cd7603efd4cf53faae5dfc4d
Tincho44 <martinharbuet@gmail.com>
2022-11-06 00:51:48 -0300

    Se commitea lo random de items

--- src/Library/scenarios/ChapterOne.cs
+++ src/Library/scenarios/ChapterOne.cs
@@ -19,6 +19,8 @@ namespace RoleplayGame.Scenarios
         var martinsito = CharacterFactory.GetCharacter(CharacterType.Elf, "martinsito");
         var pintonio = CharacterFactory.GetCharacter(CharacterType.Human, "Pintonio");
         var juancito = CharacterFactory.GetCharacter(CharacterType.Wizard, "Juancito");
+        var antoncito = CharacterFactory.GetCharacter(CharacterType.Necromancer, "antoncito");
+
         
         //crea las instancias de items del items factory
         var stick = ItemFactory.GetItem(ItemType.Robes);
@@ -28,23 +30,29 @@ namespace RoleplayGame.Scenarios
         characters.Add(martinsito);
         characters.Add(pintonio);
         characters.Add(juancito);
-
+        characters.Add(antoncito);
 
          //Random random = new Random();
          //int numeroAleatorio = random.Next(0, );
-         //Martinsito.AddItem(numeroAletorio);
+         
+
+
+          //Con esto generamos numeros aleatorios para seleccionar id de ITEMS:
+          Random rnd = new Random();
+
+   
+         int randIndex = rnd.Next(items.Count);
+
+        IItem random = items[randIndex];
+ 
+         martinsito.AddItem(random);
+         martinsito.AddItem(random);
 
        }
 
 
       // tenemos que simular todo lo que hacemos en program aca 
        public void Run(){
-
-
-        //Con esto generamos numeros aleatorios para seleccionar id de ITEMS:
-
-          Random rnd = new Random();
-          int aleatorio = rnd.Next(1, 15 + 1);
            
          // characters[aleatorio].ReceiveAttack(characters[1]);
           
