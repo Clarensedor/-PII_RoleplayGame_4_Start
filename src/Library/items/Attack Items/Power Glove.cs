2aa597de85eb05f52d7ee4105ae6c18eac509ab0
El Wombokombo <amadeo.mombru@correo.ucu.edu.uy>
2022-11-05 21:21:05 -0300

    Creación del guante de poder

---
+++ src/Library/items/Attack Items/Power Glove.cs
@@ -0,0 +1,43 @@
+namespace RoleplayGame
+{
+    public class PowerGlove: IAttackItem, IItemWithNotch
+    {
+
+        public IGemstone gemNotch {get; set;}
+        public bool gemstoneNotchIsUsed {get;set;}
+        public int AttackValue 
+        {
+            get
+            {
+                return 60;
+            } 
+            set{
+                this.AttackValue = value;
+            }
+        }
+
+
+        public int id 
+        {
+            get
+            {
+                return 5;
+            } 
+        }
+        public void AttachGem(IGemstone gem){
+
+            this.gemNotch = gem;
+            this.AttackValue += gem.value;
+            this.gemstoneNotchIsUsed = true;
+
+        }
+
+        public void DeAttachGem(){
+            this.AttackValue -= gemNotch.value;
+            this.gemstoneNotchIsUsed = false;
+
+        }
+
+        
+    }
+}
